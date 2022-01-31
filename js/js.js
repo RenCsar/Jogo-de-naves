882eff0b4013a8dce105832e030c647d691ca45f
Renan Cesar <cesar.renan@hotmail.com>
2022-01-31 19:37:06 -0300

    Adicionando a funçãPara mover o jogador para cima e para baixo

--- js/js.js
+++ js/js.js
@@ -10,19 +10,39 @@ function start() {
   //Principais variáveis do jogo
 	
 	var jogo = {}
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+  };
+  
+  jogo.pressionou = [];
 	
-	//Game Loop
+  	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+    });
+  
+  
+  $(document).keyup(function(e){
+    jogo.pressionou[e.which] = false;
+  });
+	
+
+    //Game Loop
 
 	jogo.timer = setInterval(loop,30);
 	
 	function loop() {
 	
 	movefundo();
+  movejogador();
 	
 	} // Fim da função loop()
 
   
-//Função que movimenta o fundo do jogo
+  //Função que movimenta o fundo do jogo
 	
 	function movefundo() {
 	
@@ -30,5 +50,27 @@ function start() {
     $("#fundoGame").css("background-position",esquerda-5); // para alterar a velocidade mude "esquerda-5"
     
     } // fim da função movefundo()
+
+    function movejogador() {
+	
+      if (jogo.pressionou[TECLA.W]) {
+        var topo = parseInt($("#jogador").css("top"));
+        $("#jogador").css("top",topo-10);
+      
+      }
+      
+      if (jogo.pressionou[TECLA.S]) {
+        
+        var topo = parseInt($("#jogador").css("top"));
+        $("#jogador").css("top",topo+10);	
+      }
+      
+      if (jogo.pressionou[TECLA.D]) {
+        
+        //Chama função Disparo	
+      }
+    
+      } // fim da função movejogador()
+    
 
 }
