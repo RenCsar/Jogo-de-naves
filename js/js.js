6c0b84573cbf8409b7bbc371aec38cc985c72ea5
Renan Cesar <cesar.renan@hotmail.com>
2022-02-01 11:14:38 -0300

    Movimentando helicóptero inimigo

--- js/js.js
+++ js/js.js
@@ -9,6 +9,8 @@ function start() {
   //Principais variáveis do jogo
 
   var jogo = {}
+  var velocidade=15;
+  var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
     W: 87,
     S: 83,
@@ -32,8 +34,9 @@ function start() {
   jogo.timer = setInterval(loop, 30)
 
   function loop() {
-    movefundo()
-    movejogador()
+    movefundo();
+    movejogador();
+    moveinimigo1();
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -70,4 +73,17 @@ function start() {
     }
   } // fim da função movejogador()
 
+  function moveinimigo1() {
+
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left",posicaoX-velocidade);
+    $("#inimigo1").css("top",posicaoY);
+      
+      if (posicaoX<=0) {
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left",694);
+      $("#inimigo1").css("top",posicaoY);
+        
+      }
+  } //Fim da função moveinimigo1()
 }
