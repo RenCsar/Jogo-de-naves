4be35b53e20d9a39c371f4601e3d54901f44fbde
Renan Cesar <cesar.renan@hotmail.com>
2022-02-01 12:18:21 -0300

    Adicionando Colisão

--- js/js.js
+++ js/js.js
@@ -41,6 +41,7 @@ function start() {
     moveinimigo2();
     moveamigo();
     disparo();
+    colisao();
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -93,59 +94,105 @@ function start() {
 
   function moveinimigo2() {
     posicaoX = parseInt($("#inimigo2").css("left"));
-$("#inimigo2").css("left",posicaoX-3);
+    $("#inimigo2").css("left",posicaoX-3);
     
-if (posicaoX<=0) {
-  
-$("#inimigo2").css("left",775);
+    if (posicaoX<=0) {
       
-}
-} // Fim da função moveinimigo2()
+      $("#inimigo2").css("left",775);
+          
+    }
+  } // Fim da função moveinimigo2()
 
-function moveamigo() {
-	
-	posicaoX = parseInt($("#amigo").css("left"));
-	$("#amigo").css("left",posicaoX+1);
-				
-		if (posicaoX>906) {
-			
-		$("#amigo").css("left",0);
-					
-		}
+  function moveamigo() {
+    
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left",posicaoX+1);
+          
+      if (posicaoX>906) {
+        
+      $("#amigo").css("left",0);
+            
+    }
 
-} // fim da função moveamigo()
+  } // fim da função moveamigo()
 
-function disparo() {
-	
-	if (podeAtirar==true) {
-		
-	podeAtirar=false;
-	
-	topo = parseInt($("#jogador").css("top"))
-	posicaoX= parseInt($("#jogador").css("left"))
-	tiroX = posicaoX + 190;
-	topoTiro=topo+37;
-	$("#fundoGame").append("<div id='disparo'></div");
-	$("#disparo").css("top",topoTiro);
-	$("#disparo").css("left",tiroX);
-	
-	var tempoDisparo=window.setInterval(executaDisparo, 15); // Para alterar a velocidade do disparo mude o valor 15
-	
-	} //Fecha podeAtirar
+  function disparo() {
+    
+    if (podeAtirar==true) {
+      
+      podeAtirar=false;
+      
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX= parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro=topo+37;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top",topoTiro);
+      $("#disparo").css("left",tiroX);
+      
+      var tempoDisparo=window.setInterval(executaDisparo, 15); // Para alterar a velocidade do disparo mude o valor 15
+    
+    } //Fecha podeAtirar
  
-   	    function executaDisparo() {
-	    posicaoX = parseInt($("#disparo").css("left"));
-	    $("#disparo").css("left",posicaoX+20); // Também altera a velocidade do disparo
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left",posicaoX+20); // Também altera a velocidade do disparo
 
-        		if (posicaoX>900) {
-						
-			window.clearInterval(tempoDisparo);
-			tempoDisparo=null;
-			$("#disparo").remove();
-			podeAtirar=true;
-					
-                   }
-	} // Fecha executaDisparo()
-} // Fecha disparo()
+      if (posicaoX>900) {
+              
+        window.clearInterval(tempoDisparo);
+        tempoDisparo=null;
+        $("#disparo").remove();
+        podeAtirar=true;
+            
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
+
+  function colisao() {
+    var colisao1 = ($("#jogador").collision($("#inimigo1")));
+    // jogador com o inimigo1
+
+    console.log(colisao1);
+
+  } //Fim da função colisao()
+
+  function colisao() {
+    var colisao1 = ($("#jogador").collision($("#inimigo1")));
+    // jogador com o inimigo1
+      
+      if (colisao1.length>0) {
+        
+      inimigo1X = parseInt($("#inimigo1").css("left"));
+      inimigo1Y = parseInt($("#inimigo1").css("top"));
+      explosao1(inimigo1X,inimigo1Y);
+    
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left",694);
+      $("#inimigo1").css("top",posicaoY);
+      }
+    
+    } //Fim da função colisao()
+
+  //Explosão 1
+  function explosao1(inimigo1X,inimigo1Y) {
+    $("#fundoGame").append("<div id='explosao1'></div");
+    $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+    var div=$("#explosao1");
+    div.css("top", inimigo1Y);
+    div.css("left", inimigo1X);
+    div.animate({width:200, opacity:0}, "slow");
+    
+    var tempoExplosao=window.setInterval(removeExplosao, 1000);
+    
+      function removeExplosao() {
+        
+        div.remove();
+        window.clearInterval(tempoExplosao);
+        tempoExplosao=null;
+        
+      }
+      
+    } // Fim da função explosao1()  
 
 }
