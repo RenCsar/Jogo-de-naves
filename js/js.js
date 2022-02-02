739ee84dc199036aebeb09682e54592c910f9a33
Renan Cesar <cesar.renan@hotmail.com>
2022-02-02 20:49:59 -0300

    Reiniciando o jogo

--- js/js.js
+++ js/js.js
@@ -405,9 +405,38 @@ function start() {
       $("#energia").css("background-image", "url(imgs/energia0.png)");
       
       //Game Over
+      gameOver();
     }
 
-} // Fim da função energia()
+  } // Fim da função energia()
 
+
+  //Função GAME OVER
+	function gameOver() {
+    fimdejogo=true;
+    musica.pause();
+    somGameover.play();
+    
+    window.clearInterval(jogo.timer);
+    jogo.timer=null;
+    
+    $("#jogador").remove();
+    $("#inimigo1").remove();
+    $("#inimigo2").remove();
+    $("#amigo").remove();
+    
+    $("#fundoGame").append("<div id='fim'></div>");
+    
+    $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div style='cursor: Pointer;' id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+  } // Fim da função gameOver();
+
+  
 
 }
+
+function reiniciaJogo() {
+	somGameover.pause();
+	$("#fim").remove();
+	start();
+	
+} //Fim da função reiniciaJogo
